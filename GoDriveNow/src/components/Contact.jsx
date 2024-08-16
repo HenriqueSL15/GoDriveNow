5b012526ea85a626f32de69e7990020a0bb89f77
Henrique Siqueira Lima <henrynyyls@gmail.com>
2024-08-16 19:28:07 -0300

    I started working on the contact form(when you submit it)

--- GoDriveNow/src/components/Contact.jsx
+++ GoDriveNow/src/components/Contact.jsx
@@ -1,4 +1,26 @@
 function Contact({ id }) {
+  function submitContactInfo(e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    const checkbox = document.getElementById("checkbox");
+
+    if (checkbox.checked) {
+      const fullMessage = {
+        name: name,
+        email: email,
+        message: message,
+      };
+    } else {
+      console.log(
+        "Dados não foram enviados. O usuário não marcou a caixa de acordo com os termos"
+      );
+    }
+  }
+
   return (
     <div id={id} className="flex flex-col justify-center">
       <div className="flex text-center items-center flex-col mb-20">
@@ -10,12 +32,13 @@ function Contact({ id }) {
           assistência, entre em contato conosco.
         </p>
       </div>
-      <div className="flex w-full flex-col gap-6">
+      <form className="flex w-full flex-col gap-6">
         <div className="w-full flex flex-col items-center">
           <label className="w-1/3 font-title font-semibold text-lg">Nome</label>
           <input
             className="w-1/3 px-2 py-3 font-text border border-1 border-black"
             type="text"
+            id="name"
           ></input>
         </div>
         <div className="w-full flex flex-col items-center">
@@ -25,6 +48,7 @@ function Contact({ id }) {
           <input
             className="w-1/3 px-2 py-3 font-text border border-1 border-black"
             type="text"
+            id="email"
           ></input>
         </div>
         <div className="w-full flex flex-col items-center">
@@ -34,20 +58,24 @@ function Contact({ id }) {
           <textarea
             className="w-1/3 h-56 px-2 py-3 font-text border border-1 border-black"
             type="text"
+            id="message"
           ></textarea>
         </div>
         <div className="flex flex-col">
           <div className="flex justify-center mb-4">
-            <input type="checkbox" className="mr-2 " />
+            <input id="checkbox" type="checkbox" required className="mr-2 " />
             <label>Eu aceito os Termos</label>
           </div>
           <div className="flex justify-center">
-            <button className="text-white border-2 border-transparent bg-black transition-all hover:text-black  hover:bg-white hover:border-black w-24 h-10">
+            <button
+              className="text-white border-2 border-transparent bg-black transition-all hover:text-black  hover:bg-white hover:border-black w-24 h-10"
+              onClick={submitContactInfo}
+            >
               Enviar
             </button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
